af955f060ebbe23c79c84cdc09739306765d5c80
Ajjit Narayanan <ajjit123@gmail.com>
2020-08-26 16:53:30 -0400

    Fix order of maps

--- scripts/eda-maps/iterate.R
+++ scripts/eda-maps/iterate.R
@@ -12,26 +12,24 @@ all_counties = tigris::counties(cb = TRUE, year = 2018, class = "sf") %>%
 # mapview(all_counties)
 
 # create an index
-county_geoid_list <- c("06085")
+county_geoid_list <- c("35015", "06085")
 
-all_counties %>% 
-  filter(GEOID %in% county_geoid_list)
+county_geoid_list <- sort(county_geoid_list, decreasing = FALSE)
+fnames = all_counties %>% 
+  filter(GEOID %in% county_geoid_list) %>% 
+  arrange(desc(NAME)) %>% 
+  pull(NAME)
+  
 
 # create a data frame with parameters and output file names
 runs <- tibble(
   filename = str_c(fnames, ".html"),             # creates a string with output file names in the form <index>.pdf
   params = map(county_geoid_list, ~list(county_geoid = .)))  # creates a nest list of parameters for each object in the index
 
-dir.create("factsheets/", showWarnings = FALSE)
-dir.create("appendix-factsheets/", showWarnings = FALSE)
+dir.create("scripts/eda-maps/maps/", showWarnings = FALSE)
 
 # iterate render() along the tibble of parameters and file names, which outputs
 # files into the factsheets folder
 runs %>%
   select(output_file = filename, params) %>%
-  pwalk(rmarkdown::render, input = "simple-factsheet.Rmd", output_dir = "factsheets")
-
-# Also iterate appendix factsheets
-runs %>%
-  select(output_file = filename, params) %>%
-  pwalk(rmarkdown::render, input = "appendix-factsheet.Rmd", output_dir = "appendix-factsheets")
+  pwalk(rmarkdown::render, input = "scripts/eda-maps/county_maps.rmd", output_dir = "scripts/eda-maps/maps/")
