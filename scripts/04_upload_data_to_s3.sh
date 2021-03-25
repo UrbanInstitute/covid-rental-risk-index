21b5be489ee7070fb445a43b20adc5d22a3b4750
Ajjit Narayanan <ajjit123@gmail.com>
2021-03-25 12:03:37 -0400

    Set public-read ACL access for data catalog links

--- scripts/04_upload_data_to_s3.sh
+++ scripts/04_upload_data_to_s3.sh
@@ -8,8 +8,14 @@ export $(egrep -v '^#' .env | xargs)
 
 # CSV for Data Catalog
 aws s3 cp   data/intermediate-data/housing_index_state_adj.csv "s3://${covid_rental_risk_bucket}/housing_index_state_adj.csv"
+aws s3api put-object-acl --acl public-read --bucket "${covid_rental_risk_bucket}" --key housing_index_state_adj.csv
+
+
 # Geojson for data catalog
 aws s3 cp data/intermediate-data/housing_index_state_adj.geojson "s3://${covid_rental_risk_bucket}/housing_index_state_adj.geojson"
+aws s3api put-object-acl --acl public-read --bucket "${covid_rental_risk_bucket}" --key housing_index_state_adj.geojson
+
+
 # Geojson for feature
 aws s3 cp data/intermediate-data/housing_index_state_adj_feature.geojson "s3://${covid_rental_risk_bucket}/housing_index_state_adj_feature.geojson"
 
