e004d543af2ee3c3500ef1fb4cabe3ee4ccbc26b
abwilliams13 <67596122+abwilliams13@users.noreply.github.com>
2021-03-30 11:16:46 -0400

    Indicator differences calculated
    calculated difference in indicator percentiles between 2018 and 2019

--- scripts/06_Takeaways.R
+++ scripts/06_Takeaways.R
@@ -1,4 +1,4 @@
-# 2018 index values 
+# 2018 indicator values 
 ERAP_2018 <- read_csv("data/intermediate-data/housing_index_state_adj_2018.csv") 
 
 ERAP_2018_indicators <- ERAP_2018 %>%
@@ -8,24 +8,23 @@ ERAP_2018_indicators <- ERAP_2018 %>%
         ) %>%
         rename_all(paste0, "_2018")
 
-# 2019 index values
-ERAP_2019 <- read_csv("data/intermediate-data/housing_index_state_adj_2019_ajjit.csv")
+# 2019 indicator values
+ERAP_2019 <- read_csv("data/intermediate-data/housing_index_state_adj_2019.csv")
 ERAP_2019_indicators <- ERAP_2019 %>%
   select(GEOID, perc_cost_burdened_under_35k, perc_overcrowding_renter_1.50_or_more, perc_unemployed_laborforce, 
          perc_renters, perc_poverty_12mnth, perc_no_hinsure, perc_person_of_color, 
-         perc_public_assistance, perc_foreign_born,perc_30hamfi,perc_low_income_jobs_lost            
+         perc_public_assistance, perc_foreign_born, perc_30hamfi, perc_low_income_jobs_lost            
         ) %>%
          rename_all(paste0, "_2019")
 
-
-combined <- ERAP_2019_indicators %>%
+##combine 2018 and 2019 indicators into one data frame, generate difference by indicator
+indicators_combined <- ERAP_2019_indicators %>%
   left_join(ERAP_2018_indicators, by = c("GEOID_2019" = "GEOID_2018")) %>%
-  filter(!is.na(housing_index_2018) & !is.na(covid_index_2018) & !is.na(equity_index_2018) & !is.na(total_index_2018)) %>%
-  mutate(housing_diff = housing_index_2019 - housing_index_2018,
-         covid_diff = covid_index_2019 - covid_index_2018,
-         equity_diff = equity_index_2019 - equity_index_2018,
-         total_diff = total_index_2019 - total_index_2018,
-         housing_diff_quantile = housing_index_quantile_2019 - housing_index_quantile_2018,
-         covid_diff_quantile = covid_index_quantile_2019 - covid_index_quantile_2018,
-         equity_diff_quantile = equity_index_quantile_2019 - equity_index_quantile_2018,
-         total_diff_quantile = total_index_quantile_2019 - total_index_quantile_2018)
+  mutate(cost_burden_diff = perc_cost_burdened_under_35k_2019 - perc_cost_burdened_under_35k_2018,
+         overcrowding_diff = perc_overcrowding_renter_1.50_or_more_2019 - perc_overcrowding_renter_1.50_or_more_2018,
+         poverty_diff = perc_poverty_12mnth_2019 - perc_poverty_12mnth_2018,
+         renters_diff = perc_renters_2019 - perc_renters_2018,
+         unemployment_diff = perc_unemployed_laborforce_2019 - perc_unemployed_laborforce_2018,
+         jobslost_diff = perc_low_income_jobs_lost_2019 - perc_low_income_jobs_lost_2018,
+         nohinsure_diff = perc_no_hinsure_2019 - perc_no_hinsure_2018,
+        )
