1438387610361b06e9e6e903de2cb01e0683701f
Ajjit Narayanan <ajjit123@gmail.com>
2021-05-14 10:37:26 -0400

    Rename data dictionary CSV, update S3 upload script
    Upload all files to s3 which Comms uses for feature and data catalog refrences

--- scripts/04_upload_data_to_s3.sh
+++ scripts/04_upload_data_to_s3.sh
@@ -4,21 +4,35 @@
 ## Export environment variables in .env into the shell
 export $(egrep -v '^#' .env | xargs)
 
-## Write out full state adjusted data
+## For Data Catalog
 
-# CSV for Data Catalog
+# State adjusted CSV
 aws s3 cp   data/intermediate-data/housing_index_state_adj.csv "s3://${covid_rental_risk_bucket}/housing_index_state_adj.csv"
 aws s3api put-object-acl --acl public-read --bucket "${covid_rental_risk_bucket}" --key housing_index_state_adj.csv
 
 
-# Geojson for data catalog
+# State adjusted geoJSON
 aws s3 cp data/intermediate-data/housing_index_state_adj.geojson "s3://${covid_rental_risk_bucket}/housing_index_state_adj.geojson"
 aws s3api put-object-acl --acl public-read --bucket "${covid_rental_risk_bucket}" --key housing_index_state_adj.geojson
 
 
-# Geojson for feature
+# Data dictionary
+aws s3 cp data/intermediate-data/data_dictionary_housing_data_index_state_adj.csv "s3://${covid_rental_risk_bucket}/data_dictionary_housing_data_index_state_adj.csv"
+aws s3api put-object-acl --acl public-read --bucket "${covid_rental_risk_bucket}" --key data_dictionary_housing_data_index_state_adj.csv
+
+## For feature
+
+# State adjusted Geojson
 aws s3 cp data/intermediate-data/housing_index_state_adj_feature.geojson "s3://${covid_rental_risk_bucket}/housing_index_state_adj_feature.geojson"
 
+# County Geojson
+aws s3 cp data/intermediate-data/counties.geojson "s3://${covid_rental_risk_bucket}/counties.geojson"
+
+# CoC Geojson
+aws s3 cp data/intermediate-data/coc_geographies_states_split.geojson.geojson "s3://${covid_rental_risk_bucket}/coc_geographies_states_split.geojson.geojson"
+
+
+## Unused for now
 
 # CSV for Data Catalog
 aws s3 cp data/intermediate-data/housing_index_state_natl.csv "s3://${covid_rental_risk_bucket}/housing_index_state_natl.csv"
